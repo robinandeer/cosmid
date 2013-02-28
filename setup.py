51a05b6a6bd7edbd317d093e76407ccc1f039123
Robin Andeer <robin.andeer@gmail.com>
2013-02-28 10:15:51 +0100

    restructure to make installable

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'An automatic genomics database updater.',
+    'author': 'Robin Andeer',
+    'url': 'https://github.com/robinandeer/Genie.',
+    'download_url': 'https://github.com/robinandeer/Genie/archive/master.zip',
+    'author_email': 'robin.andeer@scilifelab.se',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['genie'],
+    'scripts': ['bin/update_databases.py'],
+    'name': 'Genie'
+}
+
+setup(**config)
