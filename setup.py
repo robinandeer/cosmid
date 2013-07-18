1c71f348429f73e8db0ca5652aaa8f15343a223c
Robin Andeer <robin.andeer@gmail.com>
2013-07-18 15:06:08 +0200

    restart

--- setup.py
+++ setup.py
@@ -4,18 +4,15 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'An automatic genomics database updater.',
+    'description': 'An automated genomics database updater.',
     'author': 'Robin Andeer',
     'url': 'https://github.com/robinandeer/Genie.',
     'download_url': 'https://github.com/robinandeer/Genie/archive/master.zip',
     'author_email': 'robin.andeer@scilifelab.se',
-    'version': '0.3.8',
+    'version': '0.2.0',
     'install_requires': ['nose'],
     'packages': ['genie'],
-    'scripts': [
-        'bin/update_databases.py',
-        'bin/reorganize.py'
-    ],
+    'scripts': [],
     'name': 'Genie'
 }
 
