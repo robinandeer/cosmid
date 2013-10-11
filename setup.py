91db20ff6cb568675b57825fb2e660d9987cad3c
Robin Andeer <robin.andeer@gmail.com>
2013-10-11 15:34:54 +0200

    Updated to a more modern setup.py script

--- setup.py
+++ setup.py
@@ -1,33 +1,54 @@
+# Bootstrap distribute unless already installed
+from distribute_setup import use_setuptools
+use_setuptools()
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+
 import cosmid
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+# Shortcut for publishing to Pypi
+# Source: https://github.com/kennethreitz/tablib/blob/develop/setup.py
+if sys.argv[-1] == "publish":
+  os.system("python setup.py sdist upload")
+  sys.exit()
 
-config = {
-    "name": "cosmid",
-    "description": "A database manager for genomics",
-    "author": "Robin Andeer",
-    "url": "https://github.com/robinandeer/cosmid",
-    "download_url": "https://github.com/robinandeer/cosmid/archive/master.zip",
-    "author_email": "robin.andeer@scilifelab.se",
-    "version": cosmid.__version__,
-    "install_requires": [
-        "nose",
-        "ftputil",
-        "docopt",
-        "path.py",
-        "pyyaml",
-        "fuzzywuzzy",
-        "termcolor"
-    ],
-    "packages": [
-        "cosmid",
-        "cosmid.resources",
-        "cosmid.servers"
-    ],
-    "scripts": ["scripts/cosmid"]
-}
+setup(
+  name = "cosmid",
+  version = cosmid.__version__,
+  packages = find_packages(exclude=["tests"]),
+  scripts = ["scripts/cosmid"],
 
-setup(**config)
+  # Project dependencies
+  install_requires = [
+    "ftputil",
+    "docopt",
+    "path.py",
+    "pyyaml",
+    "fuzzywuzzy",
+    "termcolor"
+  ],
+
+  # Packages required for testing
+  tests_require = [
+    "nose"
+  ],
+
+  # Metadate for upload to Pypi
+  author = "Robin Andeer",
+  author_email = "robin.andeer@scilifelab.se",
+  description = "A genomics resource manager",
+  long_description = (open('README.rst').read()),
+  license = "MIT",
+  keywords = "bioinformatics resource manager database genomics",
+  url = "https://github.com/robinandeer/cosmid",
+  download_url = "https://github.com/robinandeer/cosmid/releases",
+  classifiers = (
+    "Development Status :: 3 - Alpha",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python"
+  )
+)
